7861ac57669d605c77cee503166bfd2a31f0f2b6
Édouard Roger <edouard.roger+github@gmail.com>
2025-11-15 08:15:40 +0100

    Ajout de la gestion du debounce pour les suggestions d'adresses et amélioration de la lisibilité du code

--- src/DsfrPostale.vue
+++ src/DsfrPostale.vue
@@ -4,7 +4,7 @@
             <span class="fr-hint-text" v-show="hint">{{ hint }}</span>
         </label>
         <input type="text" class="fr-input" role="combobox" :aria-expanded="suggestions.length > 0" :id="inputId"
-            v-model="query" @input="debounceGetAdresseSuggestions" @keydown="handleKeyDown" aria-autocomplete="list"
+            v-model="query" @input="onInput" @keydown="handleKeyDown" aria-autocomplete="list"
             aria-controls="suggestions" :aria-activedescendant="activeDescendant" autocomplete="off"
             :aria-describedby="errorMessageId" :required="required" />
         <p v-if="errorMessage" class="fr-error-text" :id="errorMessageId" role="alert">{{ errorMessage }}</p>
@@ -61,6 +61,14 @@ export default defineComponent({
             type: String as PropType<string>,
             default: () => `input-${Math.random().toString(36).substr(2, 9)}`, // ID, généré aléatoirement par défaut
         },
+        debounce: {
+            type: Boolean,
+            default: true // Debounce, activé par défaut
+        },
+        debounceDelay: {
+            type: Number as PropType<number>,
+            default: 300 // Délai de debounce en ms
+        },
         hint: {
             type: String as PropType<string>, // Aide à la saisie
         },
@@ -83,30 +91,21 @@ export default defineComponent({
         const suggestions = ref<AddressFeature[]>([]); // Stocke les suggestions d'adresses
         const activeIndex = ref<number>(-1); // Index de la suggestion actuellement sélectionnée
         const currentAddresses = ref<AddressFeature[]>([]); // Stocke les adresses actuellement disponibles
-        const debounceTimeout = ref<number | null>(null); // Timeout pour le debounce
+        let debounceTimer: number | null = null; // Timer local pour le debounce
         const inputGroupRef = ref<HTMLElement | null>(null); // Référence au conteneur
         const errorMessage = ref<string>(props.errorMessage); //  Message d'erreur
 
         // Calcule l'ID de la suggestion active pour l'accessibilité
-        const activeDescendant = computed<string>(() => {
-            return activeIndex.value >= 0 ? `suggestion-${activeIndex.value}` : "";
-        });
+        const activeDescendant = computed<string>(() => activeIndex.value >= 0 ? `suggestion-${activeIndex.value}` : "");
+        const errorMessageId = computed(() => `error-message-${props.inputId}`); // L'ID du message d'erreur
+        const suggestionsId = computed(() => `suggestions-${props.inputId}`); // Celui des suggestions
 
         watch(() => props.errorMessage, (newError) => {
             errorMessage.value = newError; // Si la prop errorMessage change, on met à jour sa valeur locale
         });
 
-        // L'ID du message d'erreur
-        const errorMessageId = computed(() => `error-message-${props.inputId}`);
-
-        // Celui des suggestions
-        const suggestionsId = computed(() => `suggestions-${props.inputId}`);
-
         // Vérifie si la requête est valide
-        const isValidQuery = (query: string): boolean => {
-            const regex = /\s[a-zA-Z]/;
-            return regex.test(query);
-        };
+        const isValidQuery = (q: string) => /\s[a-zA-Z]/.test(q);
 
         // Récupère les suggestions d'adresses depuis l'API
         const getAdresseSuggestions = async () => {
@@ -137,9 +136,7 @@ export default defineComponent({
                         city: feature.properties.city,
                         citycode: feature.properties.citycode
                     },
-                    geometry: {
-                        coordinates: feature.geometry.coordinates
-                    }
+                    geometry: { coordinates: feature.geometry.coordinates }
                 }));
 
                 suggestions.value = features;
@@ -152,87 +149,70 @@ export default defineComponent({
             }
         };
 
-        // Fonction pour retarder l'appel à l'API
-        const debounceGetAdresseSuggestions = () => {
-            if (debounceTimeout.value) {
-                clearTimeout(debounceTimeout.value); // Efface le précédent timeout
-            }
-
-            // Met en place un nouveau timeout
-            debounceTimeout.value = window.setTimeout(() => {
-                getAdresseSuggestions(); // Appelle la fonction de suggestions après un délai
-            }, 300); // Délai de 300 ms
+        // Handler déclenché à chaque saisie : recourt à un appel immédiat ou à un debounce, selon la valeur de la prop éponyme
+        const onInput = () => {
+            if (!props.debounce) return getAdresseSuggestions();
+            if (debounceTimer) clearTimeout(debounceTimer);
+            debounceTimer = window.setTimeout(() => getAdresseSuggestions(), props.debounceDelay) as unknown as number;
         };
 
         // Gère les événements de touches pour la navigation dans les suggestions
         const handleKeyDown = (event: KeyboardEvent) => {
-            // Si aucune suggestion n'est affichée
             if (suggestions.value.length === 0) {
-                if (event.key === "Escape") {
-                    // Si aucune suggestion, on vide le champ de saisie
-                    query.value = "";
-                }
+                if (event.key === "Escape") query.value = ""; // Si aucune suggestion, on vide le champ de saisie
                 return; // Si aucune suggestion, on ne va pas plus loin
             }
-            if (event.key === "ArrowDown") {
-                // Sélectionne la suggestion suivante
-                activeIndex.value = (activeIndex.value + 1) % suggestions.value.length;
-            } else if (event.key === "ArrowUp") {
-                // Sélectionne la suggestion précédente
-                activeIndex.value = (activeIndex.value - 1 + suggestions.value.length) % suggestions.value.length;
-            } else if (event.key === "Enter" && activeIndex.value >= 0) {
-                // Sélectionne l'adresse active par la touche Entrer
-                selectAddress(activeIndex.value);
-            } else if (event.key === "Escape") {
-                // Masque les suggestions si la touche Échap est utilisée
-                hideSuggestions();
-            } else if (event.key === "Tab") {
-                // Masque les suggestions si la touche Tab est employée
-                hideSuggestions();
+            switch (event.key) {
+                case "ArrowDown": // Sélectionne la suggestion suivante
+                    activeIndex.value = (activeIndex.value + 1) % suggestions.value.length;
+                    break;
+                case "ArrowUp": // Sélectionne la suggestion précédente
+                    activeIndex.value = (activeIndex.value - 1 + suggestions.value.length) % suggestions.value.length;
+                    break;
+                case "Enter": // Sélectionne l'adresse active par la touche Entrer
+                    if (activeIndex.value >= 0) selectAddress(activeIndex.value);
+                    break;
+                case "Escape": // Masque les suggestions
+                case "Tab": // Masque les suggestions si la touche Tab est employée
+                    hideSuggestions();
             }
         };
 
         // Sélectionne l'adresse en fonction de l'index
         const selectAddress = (index: number) => {
-            const selected = currentAddresses.value[index].properties; // Récupère les propriétés de l'adresse sélectionnée
+            const { properties, geometry } = currentAddresses.value[index]; // Récupère les propriétés et géométrie de l'adresse sélectionnée
             const selectedAddress: SelectedAddress = {
-                label: selected.label,
-                housenumber: selected.housenumber,
-                street: selected.street,
-                postcode: selected.postcode,
-                city: selected.city,
-                citycode: selected.citycode,
-                lat: currentAddresses.value[index].geometry.coordinates[1], // Latitude
-                lng: currentAddresses.value[index].geometry.coordinates[0], // Longitude
+                label: properties.label,
+                housenumber: properties.housenumber,
+                street: properties.street,
+                postcode: properties.postcode,
+                city: properties.city,
+                citycode: properties.citycode,
+                lat: geometry.coordinates[1], // Latitude
+                lng: geometry.coordinates[0], // Longitude
             };
-            query.value = selected.label; // Met à jour la requête avec l'adresse sélectionnée
+            query.value = properties.label; // Met à jour la requête avec l'adresse sélectionnée
             suggestions.value = []; // Vide les suggestions
-
             emit("addressSelected", selectedAddress); // Émet l'adresse sélectionnée
         };
 
-        // Fonction pour masquer les suggestions
+        // Masquer les suggestions et réinitialiser l'index actif
         const hideSuggestions = () => {
-            suggestions.value = []; // Masquer les suggestions
-            activeIndex.value = -1; // Réinitialiser l'index actif
+            suggestions.value = [];
+            activeIndex.value = -1;
         };
 
-        // Fonction pour masquer les suggestions lorsque l'utilisateur clique en dehors
+        // Masquer les suggestions lorsque l'utilisateur clique en dehors
         const handleClickOutside = (event: MouseEvent) => {
             const target = event.target as HTMLElement;
-            if (inputGroupRef.value && !inputGroupRef.value.contains(target)) {
-                hideSuggestions();
-            }
+            if (inputGroupRef.value && !inputGroupRef.value.contains(target)) hideSuggestions();
         };
 
-        // Ajoute un écouteur d'événements lors du montage du composant
-        onMounted(() => {
-            document.addEventListener("click", handleClickOutside);
-        });
-
-        // Supprime l'écouteur d'événements lors de son démontage
+        onMounted(() => document.addEventListener("click", handleClickOutside)); // Ajoute un écouteur d'événements lors du montage
         onBeforeUnmount(() => {
+            // Supprime l'écouteur d'événements et nettoie le timer
             document.removeEventListener("click", handleClickOutside);
+            if (debounceTimer) clearTimeout(debounceTimer);
         });
 
         return {
@@ -240,7 +220,7 @@ export default defineComponent({
             suggestions,
             activeIndex,
             activeDescendant,
-            debounceGetAdresseSuggestions,
+            onInput,
             handleKeyDown,
             selectAddress,
             hideSuggestions,
